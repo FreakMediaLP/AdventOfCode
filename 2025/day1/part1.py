33dedbc6dbd88bd27dfbb9d054ff5de6d3cb4821
Freak <131407675+freakmedialp@users.noreply.github.com>
2025-12-01 11:16:42 +0100

    day1

---
+++ 2025/day1/part1.py
@@ -0,0 +1,33 @@
+from functions import *
+
+cur_lock = 50
+zero_count = 0
+
+for line in read_lines():
+    operation = line[0]
+    turns = int(line[1:])
+    print(f"{operation}{turns}")
+
+    match operation:
+        case "L":
+            while turns > 0:
+                cur_lock = cur_lock - 1
+                turns = turns - 1
+
+                if cur_lock < 0:
+                    cur_lock = cur_lock + 100
+        case "R":
+            while turns > 0:
+                cur_lock = cur_lock + 1
+                turns = turns - 1
+
+                if cur_lock > 99:
+                    cur_lock = cur_lock - 100
+
+    if cur_lock == 0:
+        zero_count += 1
+
+    start = cur_lock
+    print(f"-> {cur_lock}\n")
+
+print(zero_count)  # 1154
